e4279c16d696921e1b6500969a8c6ae88d94dd49
zelalem3 <zgetnet24@gmail.com>
2024-03-06 09:47:00 -0800

    edited 2 & 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['104.196.50.234', '35.237.11.53']
+env.hosts = ['142.44.167.228', '144.217.246.195']
 
 
 def do_pack():
