3b01ca91edb2066b703f3c26c638a5e9efb847f3
zelalem3 <zgetnet24@example.com>
2024-03-07 02:23:58 -0800

    2&3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['54.89.109.87', '100.25.190.21']
 
 
 def do_deploy(archive_path):
@@ -28,4 +28,3 @@ def do_deploy(archive_path):
         return True
     except:
         return False
-
